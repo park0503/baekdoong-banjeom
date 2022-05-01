c111db28cb3d70e0771183a3f4bf2c4a4e675300
ParkSang <hooo0503@gmail.com>
2022-05-02 02:51:15 +0900

    feat: initialize backend server

---
+++ bdbj-spring/src/main/java/com/example/bdbj/BdbjApplication.java
@@ -0,0 +1,13 @@
+package com.example.bdbj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdbjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdbjApplication.class, args);
+    }
+
+}
---
+++ bdbj-spring/src/test/java/com/example/bdbj/BdbjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bdbj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdbjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
